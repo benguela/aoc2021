19952c4f8719c66910e909e17e5ce8b2327fc680
pablo.casado <pablo.casado@saab.ae>
2021-12-12 11:14:10 +0400

    second part of problem 1

--- 1.jl
+++ 1.jl
@@ -1,3 +1,13 @@
-data = split(read(joinpath(@__DIR__, "./1.txt"), String), "\n")
+using RollingFunctions
+
+data = parse.(Int, split(read(joinpath(@__DIR__, "./1.txt"), String), "\n"))
 
 sum(map((x, y) -> x > y, data[2:end], data[1:end-1]))
+
+sw = rolling((x)->sum(x),data,3)
+
+sum(map((x, y) -> x > y, sw[2:end], sw[1:end-1]))
+
+
+
+
--- 1.js
+++ 1.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
 var array = fs.readFileSync('1.txt').toString().split("\n");
 
-
-console.log(array.map((x)=>{ return x*x; }));
+console.log(array.map((x,y)=>{ return x*x; }));
 
 
 
