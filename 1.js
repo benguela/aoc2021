1cfd640454cf315636c5542ea43e48d57c7e33d6
pablo.casado <pablo.casado@saab.ae>
2021-12-15 13:27:12 +0400

    solution for problem 2 parts 1 & 2, works, needs improvement

--- 1.js
+++ 1.js
@@ -1,4 +1,9 @@
 var fs = require('fs');
+var z = require('zebras')
 var array = fs.readFileSync('1.txt').toString().split("\n");
 
 console.log(array.map((x,i)=>{ return x > array[i-1] }).reduce((a,b)=>a+b,0));
+
+sw = z.rolling(z.sum, 3, array)
+
+console.log(sw.map((x,i)=>{ return x > sw[i-1] }).reduce((a,b)=>a+b,0));
