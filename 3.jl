0528452f7791540a342386fdd44f2e18f8da6acf
pablo.casado <pablo.casado@saab.ae>
2021-12-15 15:29:23 +0400

    solution to part 1 of problem 3

--- 3.jl
+++ 3.jl
@@ -1,3 +1,5 @@
 data = parse.(Int,permutedims(reduce(hcat, collect.(readlines(joinpath(@__DIR__, "./3.txt"))))))
 
-map((s) -> s > size(data)[1]/2,  sum(data,dims=1))
+gamma = BitArray(map((s) -> s > size(data)[1]/2,  sum(data,dims=1)))
+epsilon = .!gamma
+Int(gamma.chunks[1]) * Int(epsilon.chunks[1])
